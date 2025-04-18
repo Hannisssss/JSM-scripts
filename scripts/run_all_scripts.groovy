d7793199b490486d59f0e91743873284147f3d5e
Hannisssss <115423825+Hannisssss@users.noreply.github.com>
2025-04-18 23:53:42 +0200

    Update run_all_scripts.groovy

--- scripts/run_all_scripts.groovy
+++ scripts/run_all_scripts.groovy
@@ -1,1 +1,14 @@
+def runScript(scriptPath) {
+    try {
+        def script = new File(scriptPath).text
+        evaluate(script)  // Kör det externa skriptet
+        log.info "Körde skriptet: ${scriptPath}"
+    } catch (Exception e) {
+        log.error "Fel vid körning av skriptet ${scriptPath}: ${e.message}"
+    }
+}
 
+// Exempel på att köra ett skript från en lokal fil
+def scriptDirectory = "/opt/jira/scripts"
+runScript("${scriptDirectory}/deleteOldConfigurations.groovy")
+runScript("${scriptDirectory}/createPortalGroups.groovy")
