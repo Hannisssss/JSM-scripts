8a9cad8c6a5119099e5600d4bd9e3d2cc6a827fe
Hannisssss <115423825+Hannisssss@users.noreply.github.com>
2025-04-19 23:55:40 +0200

    Update Clear_project.groovy

--- scripts/Clear_project.groovy
+++ scripts/Clear_project.groovy
@@ -1,26 +1,19 @@
 import com.atlassian.jira.component.ComponentAccessor
-import com.atlassian.servicedesk.api.portal.PortalGroupService
 
-def portalGroupService = ComponentAccessor.getOSGiComponentInstanceOfType(PortalGroupService)
-def projectManager = ComponentAccessor.getProjectManager()
-def serviceDeskManager = ComponentAccessor.getOSGiComponentInstanceOfType(ServiceDeskManager)
+// Hämtar CustomFieldManager
+def customFieldManager = ComponentAccessor.getCustomFieldManager()
 
-def project = projectManager.getProjectObjByName("Felanmälan")
-if (!project) {
-    log.warn "Projektet 'Felanmälan' finns inte – avslutar scriptet."
-    return
+// Ange namnet på fältet du vill ta bort
+def fieldName = "Mitt Exempelfält"
+
+// Försök att hitta fältet med det angivna namnet
+def customField = customFieldManager.getCustomFieldObjects().find { it.name == fieldName }
+
+if (customField) {
+    log.warn("Tar bort fältet '${customField.name}' (ID: ${customField.id})")
+    customFieldManager.removeCustomField(customField)
+    return "Fältet '${fieldName}' har tagits bort."
+} else {
+    return "Fältet '${fieldName}' hittades inte."
 }
 
-def serviceDesk = serviceDeskManager.getServiceDeskForProject(project)
-if (!serviceDesk.isPresent()) {
-    log.warn "Kunde inte hämta ServiceDesk för projektet."
-    return
-}
-
-// Radera gamla portalgrupper
-portalGroupService.getPortalGroups(serviceDesk.get().id).each { portalGroup ->
-    portalGroupService.deletePortalGroup(portalGroup.id, ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser())
-    log.info "Portalgrupp '${portalGroup.name}' raderades."
-}
-
-log.info "Steg 1: Gamla konfigurationer raderade."
